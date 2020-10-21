a216b93b500d86d178729a799c83ad4cec75beba
David Serneels <david.serneels@cipalschaubroeck.be>
2020-10-21 20:42:23 +0200

    Section 15, exercise 6: solution

--- forms-td-assignment-start/src/app/app.component.ts
+++ forms-td-assignment-start/src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import {Component, ViewChild} from '@angular/core';
+import {NgForm} from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,15 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  @ViewChild('form', {static: true}) form: NgForm;
+  defaultSubscription = 'basic';
 
+  onSubmit() {
+    console.log('email address: ' + this.form.value.email);
+    console.log('subscription type: ' + this.form.value.subscription);
+    console.log('password (very secure): ' + this.form.value.password);
+    this.form.reset({
+      subscription: this.defaultSubscription
+    });
+  }
 }
